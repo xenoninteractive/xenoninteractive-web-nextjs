bac6392fb706353be6be895d13e242d4e05aeed5
Shaun Xenon Khan <shaun.khan@protonmail.com>
2023-06-26 22:20:06 +0100

    Update branding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,26 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <div className={styles.description}></div>
 
       <div className={styles.center}>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          className={styles.logoleft}
+          src="/branding/xenon-stylized-x-logo.svg"
+          alt="Xenon Logo"
+          width={152}
+          height={96}
+          priority
+        />
+        <Image
+          className={styles.logoright}
+          src="/branding/xenon-interactive-stylized-text-logo.svg"
+          alt="Xenon Interactive Logo"
+          width={275}
+          height={61}
           priority
         />
       </div>
@@ -91,5 +77,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
